28d7317a99797581f663706eb6cb2919b90d059a
Andy <Andy@DESKTOP-UQ2GL9Q>
2020-02-14 10:25:04 +0100

    Merge corre
    Conflicts:
    	src/bancsapp/CompteBancari.java

---
+++ src/animals/Animal.java
@@ -0,0 +1,37 @@
+package animals;
+
+public abstract class Animal {
+
+    private String nombre;
+
+    public Animal() {
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void comer() {
+        System.out.println(this.getNombre() + " está comiendo...");
+    }
+
+    public void caminar() {
+        System.out.println(this.getNombre() + " está caminando...");
+    }
+
+    public abstract void sonido();
+    
+    public void correr1(){
+        
+    }
+    
+    
+    
+    public void correr(){
+        System.out.println("Corre por dios");
+    }
+}
